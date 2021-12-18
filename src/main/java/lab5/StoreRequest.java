73ce042a5d69241ed52b5ebeb8a318f3d98e45f8
Parviz <alievz2011@gmail.com>
2021-12-18 04:04:52 +0300

    Scripted auto-commit on change (2021-12-18 04:04:52) by gitwatch.sh

--- src/main/java/lab5/StoreRequest.java
+++ src/main/java/lab5/StoreRequest.java
@@ -3,4 +3,7 @@ package lab5;
 public final class StoreRequest {
     private final String url;
     public StoreRequest(String url, long time) {
+        this.url = url;
+        this.time = time;
+    }
 
