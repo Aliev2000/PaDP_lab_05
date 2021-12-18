d625b643fe4ebc32a5dbe7010635d758e382f2ac
Parviz <alievz2011@gmail.com>
2021-12-18 03:56:25 +0300

    Scripted auto-commit on change (2021-12-18 03:56:25) by gitwatch.sh

--- src/main/java/lab5/CacheActor.java
+++ src/main/java/lab5/CacheActor.java
@@ -12,6 +12,10 @@ public class CacheActor extends AbstractActor{
     }
 
     private void findInCache(Pair<String, Integer> request) {
+        String url = request.first();
+        sender().tell(
+                new CacheResponse(url, data.getOrDefault(url, -1L)),
+                getSelf()
         );
     }
 
