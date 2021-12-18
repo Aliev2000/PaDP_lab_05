1872d246d18304fb8ce781df4e42c39e00590ed4
Parviz <alievz2011@gmail.com>
2021-12-18 04:01:12 +0300

    Scripted auto-commit on change (2021-12-18 04:01:12) by gitwatch.sh

--- src/main/java/lab5/CacheResponse.java
+++ src/main/java/lab5/CacheResponse.java
@@ -4,6 +4,8 @@ public final class CacheResponse {
     private final String url;
     private final Long time;
 
+    public Long getTime() {
+
     public CacheResponse(String url, Long time) {
         this.url = url;
         this.time = time;
